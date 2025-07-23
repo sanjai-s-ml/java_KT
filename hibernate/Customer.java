b36b580e159a940e2d6e7b5179ce9f93440c1845
sanjai-s-ml <Sanjai.S@Xoriant.com>
2025-07-23 17:34:08 +0530

    Create Customer.java

---
+++ hibernate/Customer.java
@@ -0,0 +1,38 @@
+import javax.persistence.*;
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    public Customer() {
+    }
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
